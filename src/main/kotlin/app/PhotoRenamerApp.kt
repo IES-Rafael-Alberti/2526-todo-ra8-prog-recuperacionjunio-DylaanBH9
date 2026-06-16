a9b0aea010a91c266e1ce8eb55dcc0b2966754b4
dbauhue1708 <dbauhue1708@g.educaand.es>
2026-06-16 12:50:59 +0200

    validacion de errores arreglada

--- src/main/kotlin/app/PhotoRenamerApp.kt
+++ src/main/kotlin/app/PhotoRenamerApp.kt
@@ -10,27 +10,31 @@ class PhotoRenamerApp {
 
     fun run(inputPath: Path) {
 
-        val fileReader = InputFileReader()
-        val photosProcessor = PhotoProcessor()
-        val writer = ScriptWriter()
-        val console = ProcessingSummaryService()
+        try {
+            val fileReader = InputFileReader()
+            val photosProcessor = PhotoProcessor()
+            val writer = ScriptWriter()
+            val console = ProcessingSummaryService()
 
-        // Leer el fichero de entrada ya validado por ArgumentParser.
-        // Validar el formato general del fichero y construir el objeto TripInput.
+            // Leer el fichero de entrada ya validado por ArgumentParser.
+            // Validar el formato general del fichero y construir el objeto TripInput.
 
-        val trip  = fileReader.leerArchivos(inputPath)
+            val trip  = fileReader.leerArchivos(inputPath)
 
-        // Procesar las fotos validas y generar el resultado con comandos y estadisticas.
+            // Procesar las fotos validas y generar el resultado con comandos y estadisticas.
 
-        val proccesedPhotos = photosProcessor.process(trip)
+            val proccesedPhotos = photosProcessor.process(trip)
 
-        // Escribir el script <lugar>.sh con los comandos mv generados.
+            // Escribir el script <lugar>.sh con los comandos mv generados.
 
-        writer.write(trip.place, proccesedPhotos.commands, inputPath)
+            writer.write(trip.place, proccesedPhotos.commands, inputPath)
 
-        // Mostrar por consola el resumen de fotos leidas, correctas y erroneas.
+            // Mostrar por consola el resumen de fotos leidas, correctas y erroneas.
 
-        console.output(proccesedPhotos, trip)
+            console.output(proccesedPhotos, trip)
+        } catch (e: Exception) {
+            println(e.message)
+        }
 
 
     }
--- src/main/kotlin/io/InputFileReader.kt
+++ src/main/kotlin/io/InputFileReader.kt
@@ -9,13 +9,13 @@ class InputFileReader {
     fun leerArchivos(file: Path): TripInput {
         val lines = file.readLines()
 
-        if (lines.size !in 3..3) {
-            throw IllegalArgumentException("El fichero debe de contener solo 3 lineas de entrada")
-        }
+        require(lines.size == 3) {"El fichero debe tener 3 lineas"}
 
         val place = lines[0]
         val smartphonePhotos = lines[1].split(" ")
         val reflexPhotos = lines[2].split(" ")
+
+        require(place.split(" ").size == 1) {"El nombre de la ciudad debes ser una sola palabra"}
 
         return TripInput(place, smartphonePhotos, reflexPhotos)
     }
